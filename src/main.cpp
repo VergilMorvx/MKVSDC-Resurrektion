beb17654df06f5242b420ac419170c5249c33ec8
Vergil <Vergil@VERGIL>
2026-09-11 04:20:37 +0300

    Initial project scaffold generated by ReXGlue init

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+// mkvdc - ReXGlue Recompiled Project
+
+#include "generated/default/mkvdc_init.h"
+
+#include "mkvdc_app.h"
+
+REX_DEFINE_APP(mkvdc, MkvdcApp::Create)
---
+++ src/mkvdc_app.h
@@ -0,0 +1,31 @@
+// mkvdc - ReXGlue Recompiled Project
+//
+// Customize your app by overriding virtual hooks from rex::ReXApp.
+
+#pragma once
+
+#include <rex/rex_app.h>
+
+class MkvdcApp : public rex::ReXApp {
+ public:
+  using rex::ReXApp::ReXApp;
+
+  static std::unique_ptr<rex::ui::WindowedApp> Create(
+      rex::ui::WindowedAppContext& ctx) {
+    return std::unique_ptr<MkvdcApp>(new MkvdcApp(ctx, "mkvdc",
+        PPCImageConfig));
+  }
+
+  // Override virtual hooks for customization:
+  // void OnPostInitLogging() override {}
+  // void OnPreSetup(rex::RuntimeConfig& config) override {}
+  // void OnLoadXexImage(std::string& xex_image) override {}
+  // void OnPostLoadXexImage() override {}
+  // void OnPostSetup() override {}
+  // void OnCreateDialogs(rex::ui::ImGuiDrawer* drawer) override {}
+  // std::unique_ptr<rex::ui::ImGuiDialog> CreateAchievementsOverlay() override;
+  // std::unique_ptr<rex::ui::AchievementNotificationDialog>
+  // CreateAchievementNotificationDialog() override;
+  // void OnShutdown() override {}
+  // void OnConfigurePaths(rex::PathConfig& paths) override {}
+};
